8d48ed67adc46eab596035bca1f2bd2361255a11
Ruminiki Schmoeller <ruminikis@gmail.com>
2015-07-23 20:09:17 -0300

    Splash Screen

--- src/br/com/milkmoney/MainApp.java
+++ src/br/com/milkmoney/MainApp.java
@@ -4,12 +4,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Locale;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Callback;
+import javafx.util.Duration;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,40 +31,129 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import br.com.milkmoney.exception.GlobalExceptionHandler;
 
 public class MainApp extends Application {
+	
+    public static final String APPLICATION_ICON = "icon.png";
+    public static final String SPLASH_IMAGE     = "logo.png";
 
+    private Pane splashLayout;
+    private ProgressBar loadProgress;
+    private Label progressText;
+    private static final int SPLASH_WIDTH = 697;
+    
 	public static Stage primaryStage;
 	public Stage dialogStage;
 	public static BorderPane rootLayout;
 	private static ApplicationContext context;
 	
 	public MainApp() {
-		context = new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml", "services.xml", "controllers.xml", "daos.xml"});
 		//ApplicationService applicationService = (ApplicationService)getBean(ApplicationService.class);
 		//applicationService.initilizeDatabase();
 	}
 
 	@Override
 	public void start(Stage primaryStage) {
-		MainApp.primaryStage = primaryStage;
-		MainApp.primaryStage.setTitle("Milk Money");
-		initRootLayout();
-	}
+		
+		final Task<ObservableList<String>> task = new Task<ObservableList<String>>() {
+            @Override
+            protected ObservableList<String> call() throws InterruptedException {
+            	context = new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml", "services.xml", "controllers.xml", "daos.xml"});
+                updateMessage("Carregando . . .");
+                for (int i = 0; i < 50; i++) {
+                    Thread.sleep(100);
+                    updateProgress(i + 1, 50);
+                    updateMessage("Carregando . . . " + (i * 2) + "%");
+                }
+                Thread.sleep(400);
+                updateMessage("Todos os arquivos foram carregados.");
+                return null;
+            }
+        };
 
-	public static void initRootLayout() {
-	    try {
+        showSplash(primaryStage,task,() -> showMainStage());
+        new Thread(task).start();
+	}
+	
+    @Override
+    public void init() {
+        loadProgress = new ProgressBar();
+        loadProgress.setPrefWidth(SPLASH_WIDTH - 20);
+        progressText = new Label("Carregando dados . . .");
+        progressText.setTextFill(Color.WHITE);
+        splashLayout = new VBox();
+        splashLayout.setPrefHeight(227);
+        ((VBox)splashLayout).setAlignment(Pos.BOTTOM_LEFT);
+        splashLayout.getChildren().addAll(progressText, loadProgress);
+        progressText.setAlignment(Pos.CENTER);
+        splashLayout.setStyle(
+                "-fx-padding: 1; " +
+                "-fx-background-image: url('logo.png'); " +		
+                "-fx-background-color: cornsilk; " +
+                "-fx-border-width:1; " +
+                "-fx-border-color: " +
+                    "linear-gradient(" +
+                        "to bottom, " +
+                        "chocolate, " +
+                        "derive(chocolate, 50%)" +
+                    ");"
+        );
+        splashLayout.setEffect(new DropShadow());
+    }
+
+    
+    private void showMainStage() {
+    	try {
+    		
 	    	FXMLLoader loader = new FXMLLoader();
 	        loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
 	        rootLayout = (BorderPane) loader.load();
+	        
+	        primaryStage = new Stage(StageStyle.DECORATED);
+	        primaryStage.getIcons().add(new Image(ClassLoader.getSystemResourceAsStream(APPLICATION_ICON)));
+	        primaryStage.setTitle("Milk Money");
+	        
 	        Scene scene = new Scene(rootLayout);
 	        scene.getStylesheets().add("style.css");
+	        
 	        primaryStage.setScene(scene);
 	        primaryStage.show();
+	        
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	    }
+    }
 
-	}
-	
+    private void showSplash(final Stage initStage,Task<?> task,InitCompletionHandler initCompletionHandler) {
+        progressText.textProperty().bind(task.messageProperty());
+        loadProgress.progressProperty().bind(task.progressProperty());
+        task.stateProperty().addListener((observableValue, oldState, newState) -> {
+            if (newState == Worker.State.SUCCEEDED) {
+                loadProgress.progressProperty().unbind();
+                loadProgress.setProgress(1);
+                initStage.toFront();
+                FadeTransition fadeSplash = new FadeTransition(Duration.seconds(1.2), splashLayout);
+                fadeSplash.setFromValue(1.0);
+                fadeSplash.setToValue(0.0);
+                fadeSplash.setOnFinished(actionEvent -> initStage.hide());
+                fadeSplash.play();
+
+                initCompletionHandler.complete();
+            } // todo add code to gracefully handle other task states.
+        });
+
+        Scene splashScene = new Scene(splashLayout);
+        initStage.initStyle(StageStyle.UNDECORATED);
+       // final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        initStage.setScene(splashScene);
+        initStage.centerOnScreen();
+        //initStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        //initStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        initStage.show();
+    }
+
+    public interface InitCompletionHandler {
+        public void complete();
+    }
+    
 	public static void resetLayout() {
 	    try {
 	    	rootLayout.getChildren().remove(1);
