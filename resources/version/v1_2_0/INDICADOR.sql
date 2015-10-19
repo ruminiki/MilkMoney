47e7ebd949131f20ff2edc6a36a7346a4f6d8d12
Ruminiki Schmoeller <ruminikis@gmail.com>
2015-10-19 09:05:42 -0200

    Indicadores

--- resources/version/v1_2_0/INDICADOR.sql
+++ resources/version/v1_2_0/INDICADOR.sql
@@ -1,4 +1,25 @@
 drop table if exists indicador;
 
+CREATE TABLE indicador (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  descricao varchar(100) DEFAULT NULL,
+  menorValorIdeal decimal(19,2) DEFAULT NULL,
+  maiorValorIdeal decimal(19,2) DEFAULT NULL,
+  valorApurado decimal(19,2) DEFAULT NULL,
+  classeCalculo varchar(100) DEFAULT NULL,
+  ordem int(11) DEFAULT NULL,
+  sigla varchar(45) DEFAULT NULL,
+  definicao text,
+  objetivo varchar(45) DEFAULT NULL COMMENT 'Indica se o objetivo é superar o valor de referência ou ficar abaixo.',
+  tipo varchar(3) DEFAULT NULL,
+  formato varchar(30) DEFAULT NULL,
+  sufixo varchar(1) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY descricao_UNIQUE (descricao),
+  UNIQUE KEY sigla_UNIQUE (sigla)
+) ENGINE=InnoDB AUTO_INCREMENT=186 DEFAULT CHARSET=latin1;
+SELECT * FROM milkmoney.indicador;
+
+
 
 
